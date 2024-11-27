d0be1e7a71909a06d87b5f1d3fafb270b213efd4
naveenmaurya7705 <naveenmaurya7705@gmail.com>
2024-11-27 11:56:55 +0530

    server created

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,13 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const cors =require('cors')
+
 const express = require("express");
 const app = express();
+
+app.use(cors());
+
 
 app.get('/',(req,res)=>{
     res.send("hello from express.")
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,12 @@
 
 const http  = require('http');
 const app = require('./app');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
-const server = http.createServr();
+server.listen(port,(req,res)=>{
+    console.log(`server is listing at port ${port}`);
+    
+})
+
 
